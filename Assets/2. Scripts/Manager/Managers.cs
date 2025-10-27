f94c8a7ac4eb88bc9c5e11b701fd7a381b667cd8
HamoHamoHamo <hamo222@naver.com>
2025-10-27 10:44:19 +0900

    feat: Managers, PoolManager 추가

---
+++ Assets/2. Scripts/Manager/Managers.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static GameObject _root;
+    private static PoolManager _pool;
+    private static GameManager _game;
+    private static UIManager _ui;
+
+    private static void Init()
+    {
+        if (_root == null)
+        {
+            _root = new GameObject("@Manager");
+            Object.DontDestroyOnLoad(_root);
+        }
+    }
+
+    private static void CreateManager<T>(ref T manager, string name) where T : Component
+    {
+        if (manager == null)
+        {
+            Init();
+            GameObject obj = new GameObject(name);
+            manager = obj.AddComponent<T>();
+            Object.DontDestroyOnLoad(obj);
+            obj.transform.SetParent(_root.transform);
+        }
+    }
+
+    public static PoolManager Pool
+    {
+        get
+        {
+            CreateManager(ref _pool, "PoolManager");
+            return _pool;
+        }
+    }
+
+    public static GameManager Game
+    {
+        get
+        {
+            CreateManager(ref _game, "GameManager");
+            return _game;
+        }
+    }
+
+    public static UIManager UI
+    {
+        get
+        {
+            CreateManager(ref _ui, "UIManager");
+            return _ui;
+        }
+    }
+}
